7d8ee2e41bf89082fc5a39f94d1f172c63e78ff4
Mateusz Niziolek <mateusz.niziolek@gmail.com>
2025-11-29 12:10:32 +0100

    Added all excercises from my current plan

--- workouts/management/commands/seed_exercises.py
+++ workouts/management/commands/seed_exercises.py
@@ -6,16 +6,39 @@ from django.utils.text import slugify
 from workouts.models import Exercise, MuscleGroup
 
 DEFAULT_EXERCISES = [
+    # Skill block (muscle-up)
+    ("Passive Hang (Zwis pasywny)", MuscleGroup.BACK, True),
+    ("Active Hang (Zwis aktywny)", MuscleGroup.BACK, True),
+    ("Scapular Pull-Up", MuscleGroup.BACK, True),
+    ("Band Pull-Apart", MuscleGroup.SHOULDERS, True),
+    ("External Rotation", MuscleGroup.SHOULDERS, False),
+    ("Australian Pull-Up", MuscleGroup.BACK, True),
+    ("Half Explosive Pull-Up", MuscleGroup.BACK, True),
+    ("Explosive Pull-Up", MuscleGroup.BACK, True),
+    ("High Pull-Up", MuscleGroup.BACK, True),
+    ("False Grip Hang", MuscleGroup.BACK, True),
+    ("Assisted Muscle-Up (Band)", MuscleGroup.BACK, True),
+    # Trening A
     ("Back Squat", MuscleGroup.LEGS, False),
     ("Bench Press", MuscleGroup.CHEST, False),
     ("Barbell Row", MuscleGroup.BACK, False),
-    ("Pull-Up", MuscleGroup.BACK, True),
     ("Lateral Raise", MuscleGroup.SHOULDERS, False),
+    ("Dips", MuscleGroup.CHEST, True),
     ("Biceps Curl", MuscleGroup.BICEPS, False),
     ("Triceps Extension", MuscleGroup.TRICEPS, False),
     ("Hip Thrust", MuscleGroup.LEGS, False),
     ("Hollow Hold", MuscleGroup.CORE, True),
     ("Calf Raise", MuscleGroup.CALVES, False),
+    ("Close Grip Bench Press", MuscleGroup.TRICEPS, False),
+    # Trening B
+    ("Deadlift", MuscleGroup.BACK, False),
+    ("Overhead Press", MuscleGroup.SHOULDERS, False),
+    ("Chin-Up", MuscleGroup.BACK, True),
+    ("Plank", MuscleGroup.CORE, True),
+    # Additional
+    ("Reverse Fly", MuscleGroup.SHOULDERS, False),
+    ("Single-Arm Lateral Raise (45 Bench)", MuscleGroup.SHOULDERS, False),
+    ("Single-Arm Landmine Press", MuscleGroup.SHOULDERS, False),
 ]
 
 
